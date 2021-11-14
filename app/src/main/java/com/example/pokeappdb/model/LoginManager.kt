83320b73dc689e8e44601befb6d7686df84e566a
rod2arroyo <20180138@aloe.ulima.edu.pe>
2021-11-14 01:52:50 -0500

    Control de usuarios

---
+++ app/src/main/java/com/example/pokeappdb/model/LoginManager.kt
@@ -0,0 +1,34 @@
+package com.example.pokeappdb.model
+
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+// Singleton
+class LoginManager {
+    companion object{
+        var instance : LoginManager = LoginManager()
+            private set
+    }
+
+    private val dbFirebase = Firebase.firestore
+
+    fun guardarUsuario(nombre: String,
+                       callbackOK: (Long) -> Unit,
+                       callbackError: (String) -> Unit){
+
+        val data = hashMapOf<String, Any>(
+            "nombre" to nombre
+        )
+
+        val userId = System.currentTimeMillis()
+        dbFirebase.collection("usuarios")
+            .document(userId.toString())
+            .set(data)
+            .addOnSuccessListener {
+                callbackOK(userId)
+            }
+            .addOnFailureListener {
+                callbackError(it.message!!)
+            }
+    }
+}
